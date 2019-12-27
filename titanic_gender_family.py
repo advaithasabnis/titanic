eb8290a4fabdf49168c536c61d989f7afa9a2671
advaithasabnis <advait.iitb@gmail.com>
2019-12-27 01:50:51 -0500

    grouping by ticket and cleaned up feature eng

--- titanic_gender_family.py
+++ titanic_gender_family.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from pandas.api.types import CategoricalDtype
-import numpy as np
 import re
 
 #%% Import data
@@ -8,23 +7,31 @@ dataTrain = pd.read_csv('train.csv').rename(columns=lambda x: x.strip().lower())
 dataTest = pd.read_csv('test.csv').rename(columns=lambda x: x.strip().lower())
 dataBoth = [dataTrain, dataTest]
 
+#%% Treating missing data
+for data in dataBoth:
+    # Fill missing fare with 0
+    data['fare'].fillna(0, inplace=True)
+    # Fill missing cabin by other value and keep only cabin letter
+    data['cabin'].fillna('Z', inplace=True)
+    data.loc[:, 'cabin'] = data['cabin'].apply(lambda x: x[0])
+    # Fill missing embakred by most common
+    data['embarked'].fillna('S', inplace=True)
+
 #%% Feature engineering
 
-# Creating title  and lastname from name
 for data in dataBoth:
-    data['title'] = data['name'].apply(lambda x: re.search(r'\s(\w*)\.', x)[1])
+    # Create title from name and categorize into Mr, Mrs and Master
+    data.loc[:, 'title'] = data['name'].apply(lambda x: re.search(r'\s(\w*)\.', x)[1])
     data['title'] = data['title'].replace(['Capt', 'Col', 'Major', 'Don', 'Jonkheer', 'Dr', 'Rev', 'Sir'], 'Mr')
     data['title'] = data['title'].replace(['Mme', 'Countess', 'Dona', 'Lady', 'Mlle', 'Ms', 'Miss'], 'Mrs')
-    data['lastname'] = data['name'].apply(lambda x: x.split(sep=',')[0])
+    data['title'] = data['title'].astype(CategoricalDtype(categories=['Master', 'Mr', 'Mrs']))
+    # Create lastname from name
+    data.loc[:, 'lastname'] = data['name'].apply(lambda x: x.split(sep=',')[0])
 
 # Converting sex to binary
 genders = {"male": 0, "female": 1}
 for data in dataBoth:
     data['sex'] = data['sex'].map(genders)
-    
-# Conversion to category dtype
-for data in dataBoth:
-    data['title'] = data['title'].astype(CategoricalDtype(categories=['Master', 'Mr', 'Mrs']))
     
 # Dropping features
 for data in dataBoth:
@@ -32,7 +39,7 @@ for data in dataBoth:
 
 #%% Survival based on gender and family
 dataAll = pd.concat([dataTrain, dataTest], sort=False)
-dataAll.reset_index(drop=True, inplace=True)
+dataAll.reset_index(drop=True, inplace=True)    
 
 # By default assume all females lived and everyone else died
 dataAll.loc[dataAll.title!='Mrs', 'prediction'] = False
@@ -80,4 +87,3 @@ dataPred = pd.DataFrame()
 dataPred['PassengerId'] = dataTest['passengerid'].copy()
 dataPred['Survived'] = impTest.prediction
 dataPred.to_csv('submission.csv', index=False)
-
